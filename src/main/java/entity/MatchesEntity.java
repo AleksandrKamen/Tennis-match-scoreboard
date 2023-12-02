53565462c6d76ab5ff1c5d734815da53b444fa17
AleksandrKamen <Kamen.alexandr@mail.ru>
2023-12-02 12:52:27 +0300

    added MatchesEntity

---
+++ src/main/java/entity/MatchesEntity.java
@@ -0,0 +1,30 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity
+@Table(name = "matches")
+public class MatchesEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @ManyToOne
+    @JoinColumn(name = "player1", nullable = false)
+    private PlayersEntity player1;
+    @ManyToOne
+    @JoinColumn(name = "player2", nullable = false)
+    private PlayersEntity player2;
+    @ManyToOne
+    @JoinColumn(name = "winner", nullable = false)
+    private PlayersEntity winner;
+
+}
