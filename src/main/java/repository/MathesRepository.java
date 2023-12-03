ca1c0639295d85ee2e94a8f955b7534551dd9a43
AleksandrKamen <Kamen.alexandr@mail.ru>
2023-12-03 14:35:03 +0300

    added findByPlayerId method

--- src/main/java/repository/MathesRepository.java
+++ src/main/java/repository/MathesRepository.java
@@ -1,11 +1,28 @@
 package repository;
 
 import entity.MatchesEntity;
+import entity.MatchesEntity_;
+import entity.PlayersEntity;
 import jakarta.persistence.EntityManager;
+
+import java.util.List;
 
 public class MathesRepository extends BaseRepository<Integer, MatchesEntity> {
     public MathesRepository(EntityManager entityManager){
         super(MatchesEntity.class, entityManager);
+    }
+    public List<MatchesEntity> findByPlayerId(Integer playerId){
+        var entityManager = getEntityManager();
+        var playersEntity = entityManager.find(PlayersEntity.class, playerId);
+        var cb = entityManager.getCriteriaBuilder();
+        var criteria = cb.createQuery(MatchesEntity.class);
+        var matches = criteria.from(MatchesEntity.class);
+        criteria.select(matches)
+                .where(cb.or(cb.equal(matches.get(MatchesEntity_.PLAYER1),playersEntity),
+                cb.equal(matches.get(MatchesEntity_.PLAYER2),playersEntity)));
+        return entityManager.createQuery(criteria).getResultList();
+
+
     }
 
 }
