775274016a7fc2a24f20705a6f2e023ace3f8aaf
AleksandrKamen <Kamen.alexandr@mail.ru>
2023-12-03 12:43:06 +0300

    added dependencies in service Players and Matches

--- src/main/java/service/MatchesService.java
+++ src/main/java/service/MatchesService.java
@@ -1,4 +1,13 @@
 package service;
 
+import lombok.RequiredArgsConstructor;
+import mapper.matches.CreateMathesMapper;
+import mapper.matches.ReadMatchesMapper;
+import repository.MathesRepository;
+
+@RequiredArgsConstructor
 public class MatchesService {
+    private final MathesRepository mathesRepository;
+    private final ReadMatchesMapper readMatchesMapper;
+    private final CreateMathesMapper createMathesMapper;
 }
--- src/main/java/service/PlayersService.java
+++ src/main/java/service/PlayersService.java
@@ -1,4 +1,16 @@
 package service;
+
+import lombok.RequiredArgsConstructor;
+import mapper.players.CreatePlayersMapper;
+import mapper.players.ReadPlayersMapper;
+import repository.PlayersRepository;
+
+@RequiredArgsConstructor
 public class PlayersService {
+    private final PlayersRepository playersRepository;
+    private final ReadPlayersMapper readPlayersMapper;
+    private final CreatePlayersMapper createPlayersMapper;
+
+
 
 }
