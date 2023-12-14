965f34481e432f56fb9b8d4f9fbf7a02676cb993
AleksandrKamen <Kamen.alexandr@mail.ru>
2023-12-14 19:37:46 +0300

    added error in bundle

--- src/main/java/validator/players/CreatePlayerDtoValidator.java
+++ src/main/java/validator/players/CreatePlayerDtoValidator.java
@@ -8,14 +8,17 @@ import validator.Error;
 import validator.ValidationResult;
 import validator.Validator;
 
+import java.util.ResourceBundle;
+
 @AllArgsConstructor
 public class CreatePlayerDtoValidator implements Validator<CreatePlayersDto> {
    EntityManager entityManager;
     @Override
     public ValidationResult isValid(CreatePlayersDto object) {
+        var bundle = ResourceBundle.getBundle("translations");
         var validationResult = new ValidationResult();
         if (new PlayersRepository(entityManager).findByName(object.getName()).isPresent()){
-            validationResult.add(Error.of(400,"Игрок уже есть в базе данных"));
+            validationResult.add(Error.of(400,bundle.getString("error.playerDto")));
         }
         return validationResult;
     }
--- src/main/java/validator/players/PlayersNamesValidator.java
+++ src/main/java/validator/players/PlayersNamesValidator.java
@@ -5,16 +5,19 @@ import validator.ValidationResult;
 import validator.Validator;
 
 import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class PlayersNamesValidator implements Validator<List<String>> {
     @Override
     public ValidationResult isValid(List<String> names) {
-        ValidationResult validationResult = new ValidationResult();
+        var bundle = ResourceBundle.getBundle("translations", Locale.US);
+        var validationResult = new ValidationResult();
         if (!names.get(0).matches("^[a-zA-Z\s]*") || !names.get(1).matches("^[a-zA-Z\s]*")){
-            validationResult.add(Error.of(400,"Введено некорректное имя игрока(присутствуют недопустимые символы)"));
+            validationResult.add(Error.of(400, bundle.getString("error.names")));
         }
         if (names.get(0).equals(names.get(1))){
-            validationResult.add(Error.of(400,"Введено одинаковое значение имен"));
+            validationResult.add(Error.of(400, bundle.getString("error.names.duplicate")));
         }
         return validationResult;
     }
